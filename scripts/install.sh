bc60fd2a29cec09d0c7bd9deecd9dfe568db6230
Sam Smart <sam.smart@bjss.com>
2018-06-05 18:53:54 +0100

    Add dependencies to install script

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,5 +1,7 @@
 source /home/ec2-user/.bash_profile
 
+yum update -y
+yum install nodejs npm -y --enablerepo=epel
 cd /var/application/
 npm config set registry http://registry.npmjs.org/
 npm install -g pm2
